69d50f402a823882024d55756177e73a0e1a00bd
Anrijs Gedroics <anrijsgedroics@gmail.com>
2019-12-07 15:54:12 +0200

    general development on homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,16 +3,12 @@
         'Personalizēti datori' => [
             'description' => 'Neziniet kāds dators nepieciešams Jūsu vajadzībām? Neuztraucieties, Mūsu serviss nodrošinās visus rīkus, lai Jūs tiktu pie datora, kādu kārojat tieši Jūs!',
             'buttonTitle' => 'Uzzināt vairāk',
-            'buttonUrl' => 'a',
-            'imgUrl' => '',
-            'infoPlacement' => 'left',
+            'buttonUrl' => '',
         ],
         'Kontakti' => [
             'description' => '',
             'buttonTitle' => 'Uzzināt vairāk par CCS',
             'buttonUrl' => 'contacts',
-            'imgUrl' => '',
-            'infoPlacement' => 'right',
         ],
     ];
 ?>
@@ -26,27 +22,13 @@
 @endsection
 
 @section('content')
-    <?php foreach($sections as $k => $v): ?>
         <div class="row">
-            <?php if($v['infoPlacement'] == 'left'): ?>
+            <?php foreach($sections as $k => $v): ?>
                 <div class="col">
                     <h2 class="mb-3"><?= $k ?></h2>
                     <p><?= $v['description'] ?></p>
                     <a href="<?= $v['buttonUrl'] ?>" class="bg-info p-2 border-0 text-white w-100 btn"><?= $v['buttonTitle'] ?></a>
                 </div>
-                <div class="col">
-                    <img src="<?= $v['imgUrl'] ?>">
-                </div>
-            <?php else: ?>
-                <div class="col">
-                    <img src="<?= $v['imgUrl'] ?>">
-                </div>
-                <div class="col">
-                    <h2 class="mb-3"><?= $k ?></h2>
-                    <p><?= $v['description'] ?></p>
-                    <a href="<?= $v['buttonUrl'] ?>" class="bg-info p-2 border-0 text-white w-100 btn"><?= $v['buttonTitle'] ?></a>
-                </div>
-            <?php endif; ?>
+            <?php endforeach; ?>
         </div>
-    <?php endforeach; ?>
 @endsection
