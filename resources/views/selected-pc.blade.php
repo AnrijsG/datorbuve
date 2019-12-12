341df00fdf4849b4ea61b5a951ccdada8fd15447
Anrijs Gedroics <anrijsgedroics@gmail.com>
2019-12-13 00:46:00 +0200

    xd

--- resources/views/selected-pc.blade.php
+++ resources/views/selected-pc.blade.php
@@ -59,7 +59,7 @@ use App\Pc;
 
     <div class="row border mx-2 px-2 py-2">
         <div class="col-4">
-            <img class="w-100" src="https://microless.com/cdn/products/7e098f62d7dc73666e1ac86ce62fa865-md.jpg" alt="Computer case">
+            <img class="w-100" src="https://microless.com/cdn/products/7e098f62d7dc73666e1ac86ce62fa865-md.jpg" id="case-bilde" alt="Computer case">
         </div>
 
         <div class="col-8">
@@ -102,6 +102,19 @@ use App\Pc;
                 $container.addClass("d-none");
             }
         }
+
+        let caseImages = [
+            'https://www.tet.lv/veikals/i/product_popup_image/products/deepcool-tesseract-5da61d511f95a.jpg',
+            'https://microless.com/cdn/products/7e098f62d7dc73666e1ac86ce62fa865-md.jpg',
+        ];
+
+        if (!localStorage.getItem('datorbuve-case')) {
+            localStorage.setItem('datorbuve-case', 0);
+        }
+
+        localStorage.setItem('datorbuve-case', parseInt(localStorage.getItem('datorbuve-case')) + 1);
+
+        $('#case-bilde').attr('src', caseImages[localStorage.getItem('datorbuve-case') % 2]);
     </script>
     <style>
     .info{
